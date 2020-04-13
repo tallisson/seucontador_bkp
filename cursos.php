5e04b2a5f5a48b065f2ebe824d434e1ead5b23ef
Thiago Allisson <allissonribeiro02@gmail.com>
2020-04-13 06:53:19 -0300

    Salvando arqs

---
+++ cursos.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cursos | Seu Contador Resolve</title>
+<meta name="generator" content="WYSIWYG Web Builder 15 - http://www.wysiwygwebbuilder.com">
+<link href="novo_site_motta.css" rel="stylesheet">
+<link href="cursos.css" rel="stylesheet">
+<script src="jquery-1.12.4.min.js"></script>
+<script src="wb.canvas.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   $("#Shape2").DrawRectangle(
+   {
+      width: 1300,
+      height: 242,
+      strokeStyle: '#245580',
+      lineWidth: 1,
+      fillStyle: '#000000'
+   });
+});
+</script>
+</head>
+<body>
+<div id="space"><br></div>
+<div id="container">
+<div id="wb_Shape1" style="position:absolute;left:0px;top:614px;width:1300px;height:468px;z-index:0;">
+<img src="images/img0004.png" id="Shape1" alt="" style="width:1300px;height:468px;"></div>
+<div id="wb_Image1" style="position:absolute;left:23px;top:15px;width:324px;height:147px;z-index:1;">
+<a href="./index.html"><img src="images/logo_seu_Contador_Resolve.png" id="Image1" alt=""></a></div>
+<div id="RollOver1" style="position:absolute;left:1084px;top:86px;width:180px;height:50px;overflow:hidden;z-index:2">
+<a href="https://seucontadorresolve.46graus.com/contato">
+<img class="hover" alt="" src="images/contato_b.png">
+<span><img alt="" src="images/contato.png"></span>
+</a>
+</div>
+<div id="RollOver2" style="position:absolute;left:902px;top:86px;width:180px;height:50px;overflow:hidden;z-index:3">
+<a href="./negocios.php">
+<img class="hover" alt="" src="images/negocios_oportunidades_b.png">
+<span><img alt="" src="images/negocios_oportunidades.png"></span>
+</a>
+</div>
+<div id="RollOver3" style="position:absolute;left:720px;top:86px;width:180px;height:50px;overflow:hidden;z-index:4">
+<a href="./areas.html">
+<img class="hover" alt="" src="images/setores_b.png">
+<span><img alt="" src="images/setores.png"></span>
+</a>
+</div>
+<div id="RollOver4" style="position:absolute;left:538px;top:86px;width:180px;height:50px;overflow:hidden;z-index:5">
+<a href="./quemsomos.html">
+<img class="hover" alt="" src="images/quem_somos_b.png">
+<span><img alt="" src="images/quem_somos.png"></span>
+</a>
+</div>
+<div id="RollOver5" style="position:absolute;left:356px;top:86px;width:180px;height:50px;overflow:hidden;z-index:6">
+<a href="./index.html">
+<img class="hover" alt="" src="images/home_b.png">
+<span><img alt="" src="images/home.png"></span>
+</a>
+</div>
+<div id="wb_Text1" style="position:absolute;left:192px;top:266px;width:916px;height:226px;text-align:justify;z-index:7;">
+<h1>CURSOS</h1><span style="color:#000000;font-family:Arial;font-size:27px;"><br><br><br>O ser humano está em constante evolução, por isso, ministramos ou divulgamos cursos pertinetes para favorecer o seu aperfeiçoamento profissional com um conteúdo rico e defácil compreensão. <br><br>Clique no curso que mais lhe interessa e se inscrevapara participar.<br><strong>Importante: </strong>Alguns temas o levará para um ambiente fora da plataforma Seu Contador Resolve</span></div>
+<div id="wb_Image8" style="position:absolute;left:402px;top:685px;width:512px;height:285px;z-index:8;">
+<img src="images/alteracao_atos_societarios.jpg" id="Image8" alt=""></div>
+<div id="wb_Shape8" style="position:absolute;left:552px;top:1008px;width:197px;height:36px;z-index:9;">
+<a href="http://www.premiercursos.com.br/proximas-datas/alteracao-de-atos-societarios-na-pratica-" target="_blank"><img class="hover" src="images/img0044_hover.png" alt="" style="border-width:0;width:197px;height:36px;"><span class="default"><img src="images/img0044.png" id="Shape8" alt="" style="width:197px;height:36px;"></span></a></div>
+<div id="wb_Shape11" style="position:absolute;left:0px;top:1151px;width:1300px;height:106px;z-index:10;">
+<img src="images/img0067.png" id="Shape11" alt="" style="width:1300px;height:106px;"></div>
+<div id="wb_Shape3" style="position:absolute;left:169px;top:1190px;width:180px;height:50px;z-index:11;">
+<a href="./parceiros.html"><img class="hover" src="images/img0075_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0075.png" id="Shape3" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Shape4" style="position:absolute;left:433px;top:1190px;width:180px;height:50px;z-index:12;">
+<a href="./links.html"><img class="hover" src="images/img0076_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0076.png" id="Shape4" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Shape7" style="position:absolute;left:695px;top:1189px;width:180px;height:50px;z-index:13;">
+<a href="./cadastro_consultores.html"><img class="hover" src="images/img0078_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0078.png" id="Shape7" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Shape5" style="position:absolute;left:962px;top:1189px;width:180px;height:50px;z-index:14;">
+<a href=""><img class="hover" src="images/img0079_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0079.png" id="Shape5" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Text6" style="position:absolute;left:354px;top:149px;width:908px;height:15px;text-align:right;z-index:15;">
+<span style="color:#10406E;font-family:Arial;font-size:16px;letter-spacing:0.08px;">Avenida Paulista, 1842 - 17° andar - Torre Norte - Bela Vista - São Paulo / SP - CEP: 01311-200 - Tel: <strong>(11) 3567 9367</strong></span></div>
+<div id="wb_Shape2" style="position:absolute;left:0px;top:1280px;width:1300px;height:242px;z-index:16;">
+<canvas id="Shape2" height="242" width="1300"></canvas></div>
+<div id="wb_Image2" style="position:absolute;left:106px;top:1304px;width:298px;height:122px;z-index:17;">
+<img src="images/logo_seu_Contador_Resolve_contBranco.png" id="Image2" alt=""></div>
+<div id="wb_Text7" style="position:absolute;left:965px;top:1397px;width:227px;height:10px;text-align:center;z-index:18;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:11px;">Seu Contador Resolve - Todos os direitos reservados</span></div>
+<div id="wb_Image6" style="position:absolute;left:1047px;top:1316px;width:63px;height:63px;z-index:19;">
+<a href="https://api.whatsapp.com/send?phone=5511997972623&text=Gostaria%20de%20mais%20informações"><img src="images/whatsapp-logo-PNG-Transparent.png" id="Image6" alt=""></a></div>
+<div id="wb_Text3" style="position:absolute;left:965px;top:1417px;width:227px;height:10px;text-align:center;z-index:20;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:11px;">CNPJ: 35.784.643/0001-58</span></div>
+<div id="wb_Text14" style="position:absolute;left:0px;top:1433px;width:1300px;height:74px;text-align:center;z-index:21;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:16px;"><strong>Visite nosso Escritório:<br></strong><br></span><span style="color:#FFFFFF;font-family:Arial;font-size:16px;letter-spacing:0.08px;">Avenida Paulista, 1842 - 17° andar - Torre Norte - Bela Vista - São Paulo / SP - CEP: 01311-200 <br>Tel: <strong>(11) 3567 9367</strong><br></span></div>
+</div>
+</body>
+</html>
---
+++ negocios.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Negócios e Oportunidades | Seu Contador Resolve</title>
+<meta name="generator" content="WYSIWYG Web Builder 15 - http://www.wysiwygwebbuilder.com">
+<link href="novo_site_motta.css" rel="stylesheet">
+<link href="negocios.css" rel="stylesheet">
+<script src="jquery-1.12.4.min.js"></script>
+<script src="wb.canvas.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   $("#Shape5").DrawRectangle(
+   {
+      width: 1300,
+      height: 242,
+      strokeStyle: '#245580',
+      lineWidth: 1,
+      fillStyle: '#000000'
+   });
+});
+</script>
+</head>
+<body>
+<div id="space"><br></div>
+<div id="container">
+<div id="wb_Shape1" style="position:absolute;left:0px;top:557px;width:1300px;height:325px;z-index:0;">
+<img src="images/img0030.png" id="Shape1" alt="" style="width:1300px;height:325px;"></div>
+<div id="wb_Text1" style="position:absolute;left:0px;top:214px;width:1300px;height:36px;text-align:center;z-index:1;">
+<h1>NEGÓCIOS &amp; OPORTUNIDADES</h1></div>
+<div id="wb_Text2" style="position:absolute;left:164px;top:314px;width:972px;height:95px;text-align:justify;z-index:2;">
+<span style="color:#000000;font-family:Arial;font-size:27px;">Mais do que uma simples Start-up, o Seu Contador Resolve é uma facilitadora de negócios, unindo quem quer vender a quem quer comprar, quem só precisa de um empurrão a quem gosta de aproveitar boas oportunidades de investir e empreendedores que desenvolveram inovações e querem vender as idéias ou empresas a quem tem mais fôlego no mercado.</span></div>
+<div id="wb_Shape4" style="position:absolute;left:956px;top:969px;width:180px;height:50px;z-index:3;">
+<a href=""><img class="hover" src="images/img0040_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0040.png" id="Shape4" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Shape7" style="position:absolute;left:689px;top:969px;width:180px;height:50px;z-index:4;">
+<a href="./cadastro_consultores.html"><img class="hover" src="images/img0041_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0041.png" id="Shape7" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Shape2" style="position:absolute;left:163px;top:970px;width:180px;height:50px;z-index:5;">
+<a href="./parceiros.html"><img class="hover" src="images/img0048_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0048.png" id="Shape2" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Image1" style="position:absolute;left:32px;top:14px;width:324px;height:147px;z-index:6;">
+<a href="./index.html"><img src="images/logo_seu_Contador_Resolve.png" id="Image1" alt=""></a></div>
+<div id="RollOver1" style="position:absolute;left:1093px;top:85px;width:180px;height:50px;overflow:hidden;z-index:7">
+<a href="https://seucontadorresolve.46graus.com/contato">
+<img class="hover" alt="" src="images/contato_b.png">
+<span><img alt="" src="images/contato.png"></span>
+</a>
+</div>
+<div id="RollOver2" style="position:absolute;left:911px;top:85px;width:180px;height:50px;overflow:hidden;z-index:8">
+<a href="./negocios.php">
+<img class="hover" alt="" src="images/negocios_oportunidades_b.png">
+<span><img alt="" src="images/negocios_oportunidades.png"></span>
+</a>
+</div>
+<div id="RollOver3" style="position:absolute;left:729px;top:85px;width:180px;height:50px;overflow:hidden;z-index:9">
+<a href="./areas.html">
+<img class="hover" alt="" src="images/setores_b.png">
+<span><img alt="" src="images/setores.png"></span>
+</a>
+</div>
+<div id="RollOver4" style="position:absolute;left:547px;top:85px;width:180px;height:50px;overflow:hidden;z-index:10">
+<a href="./quemsomos.html">
+<img class="hover" alt="" src="images/quem_somos_b.png">
+<span><img alt="" src="images/quem_somos.png"></span>
+</a>
+</div>
+<div id="RollOver5" style="position:absolute;left:365px;top:85px;width:180px;height:50px;overflow:hidden;z-index:11">
+<a href="./index.html">
+<img class="hover" alt="" src="images/home_b.png">
+<span><img alt="" src="images/home.png"></span>
+</a>
+</div>
+<div id="wb_Image3" style="position:absolute;left:291px;top:629px;width:180px;height:180px;z-index:12;">
+<a href="./mercado-do-sertao.html"><img src="images/01_agro.png" id="Image3" alt=""></a></div>
+<div id="wb_Image4" style="position:absolute;left:561px;top:629px;width:180px;height:180px;z-index:13;">
+<a href="./investidor_anjo.html"><img src="images/anjo.png" id="Image4" alt=""></a></div>
+<div id="wb_Image5" style="position:absolute;left:830px;top:629px;width:180px;height:180px;z-index:14;">
+<a href="./compra_venda.html"><img src="images/compra_venda.png" id="Image5" alt=""></a></div>
+<div id="wb_Shape3" style="position:absolute;left:427px;top:970px;width:180px;height:50px;z-index:15;">
+<a href="./links.html"><img class="hover" src="images/img0042_hover.png" alt="" style="border-width:0;width:180px;height:50px;"><span class="default"><img src="images/img0042.png" id="Shape3" alt="" style="width:180px;height:50px;"></span></a></div>
+<div id="wb_Text6" style="position:absolute;left:363px;top:147px;width:908px;height:15px;text-align:right;z-index:16;">
+<span style="color:#10406E;font-family:Arial;font-size:16px;letter-spacing:0.08px;">Avenida Paulista, 1842 - 17° andar - Torre Norte - Bela Vista - São Paulo / SP - CEP: 01311-200 - Tel: <strong>(11) 3567 9367</strong></span></div>
+<div id="wb_Shape5" style="position:absolute;left:0px;top:1074px;width:1300px;height:242px;z-index:17;">
+<canvas id="Shape5" height="242" width="1300"></canvas></div>
+<div id="wb_Image2" style="position:absolute;left:106px;top:1098px;width:298px;height:122px;z-index:18;">
+<img src="images/logo_seu_Contador_Resolve_contBranco.png" id="Image2" alt=""></div>
+<div id="wb_Text7" style="position:absolute;left:965px;top:1191px;width:227px;height:10px;text-align:center;z-index:19;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:11px;">Seu Contador Resolve - Todos os direitos reservados</span></div>
+<div id="wb_Image6" style="position:absolute;left:1047px;top:1110px;width:63px;height:63px;z-index:20;">
+<a href="https://api.whatsapp.com/send?phone=5511997972623&text=Gostaria%20de%20mais%20informações"><img src="images/whatsapp-logo-PNG-Transparent.png" id="Image6" alt=""></a></div>
+<div id="wb_Text3" style="position:absolute;left:965px;top:1211px;width:227px;height:10px;text-align:center;z-index:21;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:11px;">CNPJ: 35.784.643/0001-58</span></div>
+<div id="wb_Text14" style="position:absolute;left:0px;top:1227px;width:1300px;height:74px;text-align:center;z-index:22;">
+<span style="color:#FFFFFF;font-family:Arial;font-size:16px;"><strong>Visite nosso Escritório:<br></strong><br></span><span style="color:#FFFFFF;font-family:Arial;font-size:16px;letter-spacing:0.08px;">Avenida Paulista, 1842 - 17° andar - Torre Norte - Bela Vista - São Paulo / SP - CEP: 01311-200 <br>Tel: <strong>(11) 3567 9367</strong><br></span></div>
+</div>
+</body>
+</html>
